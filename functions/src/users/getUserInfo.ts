b2fa678d40af2ec4ca1cf818e37fe1cc9646c426
hiromuu <hiromu2427@gmail.com>
2023-09-04 22:45:47 +0900

    deployが上手くいかない

--- functions/src/users/getUserInfo.ts
+++ functions/src/users/getUserInfo.ts
@@ -1,17 +1,21 @@
-import * as admin from 'firebase-admin';
+import * as admin from "firebase-admin";
+import { CallableContext } from "firebase-functions/v1/https";
 
 const db = admin.firestore();
 
-export const getUserInfo = async (data, context) => {
-    const userId = context.auth?.uid;
-    if (!userId) {
-        throw new Error('User is not authenticated.');
-    }
+export const getUserInfo = async (
+  data: Record<string, unknown>,
+  context: CallableContext
+) => {
+  const userId = context.auth?.uid;
+  if (!userId) {
+    throw new Error("User is not authenticated.");
+  }
 
-    const userDoc = await db.collection('Users').doc(userId).get();
-    if (!userDoc.exists) {
-        throw new Error('User not found.');
-    }
+  const userDoc = await db.collection("Users").doc(userId).get();
+  if (!userDoc.exists) {
+    throw new Error("User not found.");
+  }
 
-    return userDoc.data();
+  return userDoc.data();
 };
